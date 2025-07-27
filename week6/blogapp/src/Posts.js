e4e96bafd3bedd0caae2078aba097a8b090375d8
Nikhita-98 <nikhitap.1908@gmail.com>
2025-07-27 23:37:36 +0530

    Week 6

---
+++ week6/blogapp/src/Posts.js
@@ -0,0 +1,56 @@
+// src/Posts.js
+import React from 'react';
+import Post from './Post';
+
+class Posts extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+      hasError: false
+    };
+  }
+
+  loadPosts() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(data => {
+        const posts = data.map(post => new Post(post.id, post.title, post.body));
+        this.setState({ posts });
+      })
+      .catch(error => {
+        console.error('Error fetching posts:', error);
+        this.setState({ hasError: true });
+      });
+  }
+
+  componentDidMount() {
+    this.loadPosts();
+  }
+
+  componentDidCatch(error, info) {
+    alert("An error occurred while rendering the component.");
+    console.error("Error caught:", error, info);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h2>Something went wrong while loading posts.</h2>;
+    }
+
+    return (
+      <div>
+        <h1>Blog Posts</h1>
+        {this.state.posts.map(post => (
+          <div key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+            <hr />
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Posts;
